9d2e38739187c45935ace10199075e782bd54187
saurav4986 <saurav.raj4986@gmail.com>
2022-11-18 17:19:16 +0530

    Added: codes to check either win or loose

--- src/com/bridgeLabz/Gambling.java
+++ src/com/bridgeLabz/Gambling.java
@@ -1,12 +1,25 @@
 package com.bridgeLabz;
-/*(UC1-Game_Bet)
-As a Gambler, would start with a stake of
-$100 every day and bet $1 every game.*/
+/*(UC2-Game_Start)
+As a Gambler make $1 bet
+so either win or loose $1*/
 
 public class Gambling {
     static int STAKE = 100;
     static int BET = 1;
     public static void main(String[] args) {
         System.out.println("!!!Welcome to the world of Gambling!!!");
+        int result;
+        int opt = (int) (Math.random() * 2);
+
+        System.out.println("Your Initial Balance: $"+STAKE);
+        if (opt == 1){
+            System.out.println("You Won the Game");
+            result = STAKE + BET;
+        }
+        else {
+            System.out.println("You Lost the Game");
+            result = STAKE - BET;
+        }
+        System.out.println("Your Final Balance: $"+result);
     }
 }
